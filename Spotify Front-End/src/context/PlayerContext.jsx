8dd8951b4e48cfbd1de2c027c0c8088dd8892916
Arbab Kamal <146167220+wingie125@users.noreply.github.com>
2024-07-29 14:06:09 +0530

    Update PlayerContext.jsx

--- Spotify Front-End/src/context/PlayerContext.jsx
+++ Spotify Front-End/src/context/PlayerContext.jsx
@@ -9,7 +9,7 @@ const PlayerContextProvider = (props) => {
   const seekBg = useRef();
   const seekBar = useRef();
 
-  const url = "http://localhost:4000";
+  const url = "https://spotify-clone-backend-8x9n.onrender.com";
   const [songsData, setSongsData] = useState([]);
   const [albumsData, setAlbumsData] = useState([]);
   const [track, setTrack] = useState(songsData[0]);
