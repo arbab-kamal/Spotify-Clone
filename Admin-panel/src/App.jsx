92e0dbcd78f6e38201448c4189bae0723200083c
Arbab Kamal <146167220+wingie125@users.noreply.github.com>
2024-07-29 14:05:22 +0530

    Update App.jsx

--- Admin-panel/src/App.jsx
+++ Admin-panel/src/App.jsx
@@ -10,7 +10,7 @@ import ListAlbum from "./pages/ListAlbum";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 
-export const url = "http://localhost:4000";
+export const url = "https://spotify-clone-backend-8x9n.onrender.com";
 
 const App = () => {
   return (
