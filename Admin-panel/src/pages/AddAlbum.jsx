7642a422dd38b0a02a97e8c60d1fe7b891edc490
wingie125 <arbab.vns123@gmail.com>
2024-08-21 00:30:25 +0530

    Final

---
+++ Admin-panel/src/pages/AddAlbum.jsx
@@ -0,0 +1,102 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import { assets } from "../assets/admin-assets/assets";
+import { toast } from "react-toastify";
+import { url } from "../App";
+import axios from "axios";
+const AddAlbum = () => {
+  const [image, setImage] = useState(false);
+  const [color, setColor] = useState("#ffffff");
+  const [name, setName] = useState("");
+  const [desc, setDesc] = useState("");
+  const [loading, setLoading] = useState(false);
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const formData = new FormData();
+      formData.append("name", name);
+      formData.append("desc", desc);
+      formData.append("image", image);
+      formData.append("bgColor", color);
+      const response = await axios.post(`${url}/api/album/add`, formData);
+      if (response.data.success) {
+        toast.success("Album Added");
+        setName("");
+        setDesc("");
+        setImage(false);
+        setColor("#ffffff");
+      } else {
+        toast.error("Something Went wrong");
+      }
+    } catch (error) {
+      toast.error("Something is Wrong");
+    }
+    setLoading(false);
+  };
+
+  return loading ? (
+    <div className="grid place-items-center min-h-[80vh]">
+      <div className="w-16 h-16 place-self-center border-4 border-gray-400 border-t-green-800 rounded-full animate-spin"></div>
+    </div>
+  ) : (
+    <form
+      onSubmit={onSubmitHandler}
+      className="flex flex-col items-start gap-8 text-gray-600"
+    >
+      <div className="flex flex-col gap-4">
+        <p>Upload Image</p>
+        <input
+          onChange={(e) => setImage(e.target.files[0])}
+          type="file"
+          id="image"
+          accept="image/*"
+          hidden
+        />
+        <label htmlFor="image">
+          <img
+            className="w-24 cursor-pointer"
+            src={image ? URL.createObjectURL(image) : assets.upload_area}
+            alt=""
+          />
+        </label>
+      </div>
+      <div className="flex flex-col gap-2.5">
+        <p>Album name</p>
+        <input
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          className="bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw,240px)] "
+          type="text"
+          placeholder="Type Here"
+        />
+      </div>
+      <div className="flex flex-col gap-2.5">
+        <p>Add description</p>
+        <input
+          onChange={(e) => setDesc(e.target.value)}
+          value={desc}
+          className="bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw,240px)] "
+          type="text"
+          placeholder="Type Here"
+        />
+      </div>
+      <div className="flex flex-col gap-3">
+        <p>Background Color</p>
+        <input
+          onChange={(e) => setColor(e.target.value)}
+          value={color}
+          type="color"
+        />
+      </div>
+      <button
+        type="submit"
+        className="text-base bg-black text-white py-2.5 px-14 cursor-pointer"
+      >
+        Add
+      </button>
+    </form>
+  );
+};
+
+export default AddAlbum;
